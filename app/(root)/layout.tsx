d4ec49f81fa3b0e2c5f72b42ac923fc3a23251fb
Jose Mokeni <jmmokeni@gmail.com>
2025-08-13 23:37:26 +0100

    light/dark toggle

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Toaster } from "@/components/ui/sonner";
+import { ModeToggle } from "@/components/theme-mode-toggle";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
 
@@ -10,7 +11,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <SidebarProvider>
         <AppSidebar />
         <main className="w-full">
-          <SidebarTrigger />
+          <div className="flex items-center justify-between p-4 border-b">
+            <SidebarTrigger />
+            <ModeToggle />
+          </div>
           <div className="p-4">{children}</div>
         </main>
         <Toaster />
